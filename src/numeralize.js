615693862327dfddfe0ccf803cccdf563175699d
Will Slotterback <slotterbackW@Will_Lenovo-PC.localdomain>
2017-06-08 10:58:59 -0700

    Update Numeralization Code
    This commit alters the data structure used to represent numerals
    in order to clean up the code for conversion. Because edge cases are
    now handled in our data structure we no longer have to handle them
    in the code.

--- src/numeralize.js
+++ src/numeralize.js
@@ -1,86 +1,55 @@
-var expect = require('expect');
+const expect = require('expect');
 
-function numeralize(num) {
-   var ds = [
-    {
-      name: 'M',
-      value: 1000
-    },
-    {
-      name: 'D',
-      value: 500
-    },
-    {
-      name: 'C',
-      value: 100
-    },
-    {
-      name: 'L',
-      value: 50
-    },
-    {
-      name: 'X',
-      value: 10
-    },
-    {
-      name: 'V',
-      value: 5
-    },
-    {
-      name: 'I',
-      value: 1
-    }
-  ];
-  var di = 0;
-  var n = num;
-  var result = '';
-  while (n != 0) {
-    var d = ds[di];
-    if (d.value <= n) {
-      if ( di % 2 == 1 && di > 0 && di < ds.length - 1) {
-        if ( n >= (ds[di - 1].value - ds[di + 1].value)) {
-          result = result + ds[di + 1].name + ds[di - 1].name;
-          n = n % (ds[di - 1].value - ds[di + 1].value);
-        } else {
-          var np = Math.floor(n / d.value);
-        	result = result + d.name.repeat(np);
-        	n = n % d.value;
-        }
-      } else if ( di % 2 == 0 && di > 0) {
-        if ( n >= (ds[di - 1].value - d.value) ) {
-          result = result + d.name + ds[di - 1].name;
-          n = n % (ds[di - 1].value - d.value);
-        } else {
-          var np = Math.floor(n / d.value);
-        	result = result + d.name.repeat(np);
-        	n = n % d.value;
-        }
-      } else {
-        var np = Math.floor(n / d.value);
-       	result = result + d.name.repeat(np);
-       	n = n % d.value;
-      }
-    }
-    di++;
-  }
-  return result;
+const numerals = {
+	1: 'I',
+	4: 'IV',
+	5: 'V',
+	9: 'IX',
+	10: 'X',
+	40: 'XL',
+	50: 'L',
+	90: 'XC',
+	100: 'C',
+	400: 'CD',
+	500: 'D',
+	900: 'CM',
+	1000: 'M'
 }
 
-// Some tests using expect library: https://github.com/mjackson/expect 
+function numeralize(num) {
+	const divisors = Object.keys(numerals);
+	let di = divisors.length - 1;
+	let result = [];
+
+	while (num > 0) {
+		if (num < divisors[di]) {
+			di--;
+		} else {
+			const divisor = divisors[di];
+			result.push(numerals[divisor]);
+			num -= divisor;
+		}
+	}
+	return result.join('');
+}
+
+// Some tests using expect library: https://github.com/mjackson/expect
 expect(numeralize(1)).toEqual('I');
 expect(numeralize(2)).toEqual('II');
 expect(numeralize(3)).toEqual('III');
+expect(numeralize(4)).toEqual('IV');
 expect(numeralize(5)).toEqual('V');
 expect(numeralize(9)).toEqual('IX');
-expect(numeralize(12)).toEqual('XII');
-expect(numeralize(16)).toEqual('XVI');
+expect(numeralize(13)).toEqual('XIII');
+expect(numeralize(14)).toEqual('XIV');
 expect(numeralize(29)).toEqual('XXIX');
 expect(numeralize(44)).toEqual('XLIV');
 expect(numeralize(45)).toEqual('XLV');
 expect(numeralize(68)).toEqual('LXVIII');
 expect(numeralize(83)).toEqual('LXXXIII');
 expect(numeralize(99)).toEqual('XCIX');
+expect(numeralize(400)).toEqual('CD');
 expect(numeralize(500)).toEqual('D');
 expect(numeralize(501)).toEqual('DI');
 expect(numeralize(3999)).toEqual('MMMCMXCIX');
-
+console.log('All tests passed!');
